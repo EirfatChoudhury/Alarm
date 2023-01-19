4083c9d142b24aa7e7aacc1c5d7e9b797bd15f29
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-01-19 17:28:41 +0000

    Taking a picture of a frame when space bar is clicked and naming it test.jpg if test.jpg does not already exist.

--- alarm.py
+++ alarm.py
@@ -1,19 +1,22 @@
-from cv2 import barcode, imread, haveImageReader, VideoCapture, imshow, waitKey
-import sys
+from cv2 import barcode, imread, haveImageReader, VideoCapture, imshow, waitKey, imwrite
 
 if not haveImageReader("test.jpg") == True:
     cam = VideoCapture(0)
     if cam.isOpened():
-        rval, frame = vc.read()
+        rval, frame = cam.read()
     else:
         rval = False
     while rval:
         imshow("preview", frame)
-        rval, frame = vc.read()
+        rval, frame = cam.read()
         key = waitKey(20)
-        if key == 27:
+        if key%256 == 32:
+            temp_rval, test_image = cam.retrieve()
+            imwrite("test.jpg", test_image)
+        elif key == 27:
             break
     cam.release()
+
 else:
     image1 = imread("barcode_test.jpg")
     image2 = imread("barcode_test2.png")
